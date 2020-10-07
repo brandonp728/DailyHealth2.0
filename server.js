78e3dd38c5c5bdf0a8550221628218a4e771d63d
php5185 <php5185@rit.edu>
2020-10-06 23:36:15 -0400

    client and server communication

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 
 app.listen(3000, () => console.log("Listening at 3000"));
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
 
-
+app.post('/api', (request, response) => {
+  console.log(request.body);
+  response.json({
+    status: 'successPaola'
+  });
+});
 // var http = require('http');
 
 // http.createServer(function (req, res) {
