032052a939668e6f8c61938bd089f32c27d144b9
mma7132 <mma7132@g.rit.edu>
2020-10-23 02:31:23 -0400

    updates

--- public/index.js
+++ public/index.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-
-
-var user;
-var pass;
- function myFunction() {
-     user = document.getElementById("username").value ;
-    console.log(user);
-    pass = document.getElementById("password").value;
-    console.log(pass);
-
-    if(pass=="" || user==""){
-        alert("Username and password must be filled out");
-        return false;
-    }
-
-  }
-
-
-  function sendAssesment(){
-      console.log("hello");
-  }
-// const test = 1.3;
-
-
-// const test2 = 5.5;
-// const data = { user, test2 };
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(data)
-// };
-// fetch('/api', options).then(response => {
-//     console.log(response);// to get the respose in nice text the function could be async and use await vid 2.3
-// });
-=======
 var currentDate = new Date().toISOString().slice(0, 10);
 document.getElementById("activityCalendar").min = currentDate;
 
@@ -57,4 +19,41 @@ function closeOpenOptions() {
     }
   });
 }
->>>>>>> 0e7419fd4a5f4b9d539cf723ae68e7e3fa42905e
+function redirect() {
+  let q1 = document.getElementById("question1").checked;
+  let q2 = document.getElementById("question2").checked;
+
+  let failed = q1 || q2;
+
+  console.warn(q1, q2);
+
+  if (document.getElementById("no_symptoms").checked == true)
+    window.location.href = "./Greencard.html";
+  else if (failed) window.location.href = "./Redcard.html";
+  else alert("No Items are selected");
+}
+
+function questionCheck(params) {
+  if (document.getElementById(params).checked === true) {
+    document.getElementById("no_symptoms").disabled = true;
+  } else {
+    document.getElementById("no_symptoms").disabled = false;
+  }
+}
+
+function symptomCheck() {
+  let inputs = document
+    .getElementById("questions")
+    .getElementsByTagName("input");
+
+  if (document.getElementById("no_symptoms").checked === true) {
+    [...inputs].forEach((element) => {
+      element.checked = false;
+      element.disabled = true;
+    });
+  } else {
+    [...inputs].forEach((element) => {
+      element.disabled = false;
+    });
+  }
+}
