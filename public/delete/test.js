c5dd7dc5ca4828030edb3a5a633c2636b110e942
php5185 <php5185@rit.edu>
2020-10-30 17:09:13 -0400

    login working

---
+++ public/delete/test.js
@@ -0,0 +1,11 @@
+row = [
+    {id: 5},
+    meta= {
+        pad:6
+
+    }
+    
+];
+
+console.log(row.meta)
+console.log(row[0])
--- public/index.js
+++ public/index.js
@@ -2,23 +2,53 @@
 
 var user;
 var pass;
- function myFunction() {
-     user = document.getElementById("username").value ;
-    console.log(user);
-    pass = document.getElementById("password").value;
-    console.log(pass);
+async function myFunction() {
+  user = document.getElementById("username").value;
+ 
+  pass = document.getElementById("password").value;
+  
 
-    if(pass=="" || user==""){
-        alert("Username and password must be filled out");
-        return false;
-    }
-
+  if (pass == "" || user == "") {
+    alert("Username and password must be filled out");
+    return false;
   }
 
-
-  function sendAssesment(){
-      console.log("hello");
+  credential = {
+    username: user,
+    password: pass
   }
+
+  //pass to server to check in database
+  // const response = await fetch('/login' );
+  // const myresponse = await response.json();
+  // console.log(myresponse);
+
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(credential)
+  };
+
+  const resp = await fetch('/login', options);
+  // .then(response => {
+  //   console.log("here");
+
+  //   console.log(response.status);// to get the respose in nice text the function could be async and use await vid 2.3// or it could be another then in the client
+  // });
+  const callback = await resp.json();
+  if(callback.status == true){
+    location.replace("http://localhost:3000/home.html")
+  }
+  console.log(callback);
+
+}
+
+
+function sendAssesment() {
+  console.log("hello");
+}
 // const test = 1.3;
 
 
@@ -32,5 +62,10 @@ var pass;
 //     body: JSON.stringify(data)
 // };
 // fetch('/api', options).then(response => {
-//     console.log(response);// to get the respose in nice text the function could be async and use await vid 2.3
+//     console.log("here")
+//     console.log(response.body);// to get the respose in nice text the function could be async and use await vid 2.3// or it could be another then in the client
 // });
+
+
+//video 2.3 send and receive data
+//video 2.4 database
